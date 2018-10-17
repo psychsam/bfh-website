f6f7447b9bd6ca8f47290753bf705b88a2f8dc06
psychsam <imboden.sam@gmail.com>
2018-10-17 10:45:09 +0200

    added style.js and changed stundenplan.html

---
+++ script.js
@@ -0,0 +1,30 @@
+function markField () {
+  let day = new Date().getDay();
+  let hours = new Date().getHours();
+  let minutes = new Date().getMinutes();
+  let daysInWeek = ['s', 'm', 'tu', 'w', 'th', 'f', 'sa'];
+  let times = [8*60+20, 10*60+20, 12*60, 12*60+45, 14*60+25, 16*60+15, 17*60+50];
+  let table = document.querySelector('table');
+  let result = 0;
+  times.forEach((element,index) => {
+    let comparedTimes = compareTime(hours*60+minutes,element,index);
+    if(comparedTimes && result === 0) {
+      result = comparedTimes;
+    }
+  });
+
+  if(result <= 7) {
+    let myClass = document.querySelector("."+daysInWeek[day] + result);
+    myClass.classList.add("rainbow");
+    table.classList.add('fancy');
+  }
+
+}
+
+function compareTime(now,time,index) {
+  let pos;
+  if(now <= time) {
+    pos = index;
+  }
+  return pos;
+}
