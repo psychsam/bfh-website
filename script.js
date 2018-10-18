7bf145ca1bdf844faa814cea4635cadf164459a3
apate2etexi <40541291+apate2etexi@users.noreply.github.com>
2018-10-18 14:09:05 +0200

    added automatic update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function markField() {
   let hours = new Date().getHours();
   let minutes = new Date().getMinutes();
   let daysInWeek = [ 's', 'm', 'tu', 'w', 'th', 'f', 'sa' ];
-  let times = [ 8 * 60 + 19, 10 * 60 + 19, 11 * 60 + 55, 12 * 60 + 44, 14 * 60 + 24, 16 * 60 + 14, 17 * 60 + 49 ];
+  let times = [ 8 * 60 + 19, 10 * 60 + 19, 11 * 60 + 55, 12 * 60 + 44, 14 * 60 + 19, 16 * 60 + 14, 17 * 60 + 49 ];
   let result = 0;
   times.forEach(( element, index ) => {
     let comparedTimes = compareTime(hours * 60 + minutes, element, index);
@@ -16,11 +16,23 @@ function markField() {
   if (result <= 7) {
     let myClass = document.querySelector("." + daysInWeek[ day ] + result);
 
-    let name = myClass.className;
-    let names = name.split(" ");
+
+    let names =  myClass.className.split(" ");
+
     if (names[ 0 ] !== "nonborder") {
       myClass.classList.add(names[ 0 ] + "_highlight");
     }
+
+    if(result > 0) {
+        let bevorClass = document.querySelector("." + daysInWeek[ day ] + (result-1));
+        let bevorenames = bevorClass.className.split(" ");
+
+        if(bevorenames.length == 3) {
+            let newname = bevorenames[0] + " " + bevorenames[1];
+            bevorClass.className = newname;
+        }
+    }
+
   }
 }
 
@@ -36,6 +48,5 @@ function callMarkField() {
   markField();
   window.setInterval(function(){
     markField();
-    console.log(1);
   }, 60000);
 }
