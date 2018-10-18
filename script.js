928630e3e40fb28489b00fc6b0121bf6238fb5c0
Julian <julian@praxishaldimann.ch>
2018-10-18 22:23:27 +0200

    Refactor code and cleanup

--- script.js
+++ script.js
@@ -2,21 +2,26 @@ function markField() {
   let day = new Date().getDay();
   let hours = new Date().getHours();
   let minutes = new Date().getMinutes();
+  // define days for html classed
   let daysInWeek = [ 's', 'm', 'tu', 'w', 'th', 'f', 'sa' ];
+  // define times for lessons
   let times = [ 8 * 60 + 19, 10 * 60 + 19, 11 * 60 + 55, 12 * 60 + 44, 14 * 60 + 19, 16 * 60 + 14, 17 * 60 + 49 ];
   let result = 0;
+  // compare every time until you receive true
+  // then print the index
   times.forEach(( element, index ) => {
     let comparedTimes = compareTime(hours * 60 + minutes, element, index);
     if (comparedTimes && result === 0) {
       result = comparedTimes;
     }
   });
-  if (result > 0) result--;
+
+  if (result > 0) {
+    result--;
+  }
 
   if (result <= 7) {
     let myClass = document.querySelector("." + daysInWeek[ day ] + result);
-
-
     let names =  myClass.className.split(" ");
 
     if (names[ 0 ] !== "nonborder") {
@@ -24,18 +29,17 @@ function markField() {
     }
 
     if(result > 0) {
-        let bevorClass = document.querySelector("." + daysInWeek[ day ] + (result-1));
-        let bevorenames = bevorClass.className.split(" ");
+      let beforeClass = document.querySelector("." + daysInWeek[ day ] + (result-1));
+      let beforeNames = beforeClass.className.split(" ");
 
-        if(bevorenames.length == 3) {
-            let newname = bevorenames[0] + " " + bevorenames[1];
-            bevorClass.className = newname;
-        }
+      if(beforeNames.length === 3) {
+          beforeClass.className = beforeNames[0] + " " + beforeNames[1];
+      }
     }
-
   }
 }
 
+// compare two times
 function compareTime( now, time, index ) {
   let pos;
   if (now <= time) {
@@ -44,6 +48,7 @@ function compareTime( now, time, index ) {
   return pos;
 }
 
+// call the function every minute
 function callMarkField() {
   markField();
   window.setInterval(function(){
